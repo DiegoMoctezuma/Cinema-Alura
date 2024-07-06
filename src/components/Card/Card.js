c003f831bf7d93f3695343a0ebe95932b8af7303
Rafael Moctezuma <moctezumarafael26@gmail.com>
2024-07-06 16:43:35 -0600

    Nuevas rutas - player

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,30 @@
+import { useFavoritosContext } from 'context/Favoritos';
 import styles from './Card.module.css';
 import iconFavorito from './iconFavorito.png';
+import iconNoFavorito from './iconNoFavorito.png';
+import { Link } from 'react-router-dom';
 
 function Card({id,capa,titulo}){
+
+    const {favoritos, agregarFavorito} = useFavoritosContext();
+    const esFavorito = favoritos.some(fav => fav.id === id);
+    const icon = esFavorito ? iconFavorito : iconNoFavorito;
+
     return(
         <div className={styles.container}>
-            <img src={capa} alt={titulo} className={styles.capa}/>
+            <Link className={styles.link} to={`/${id}`}>
+                <img src={capa} alt={titulo} className={styles.capa}/>
+            </Link>
             <div className={styles.infoContainer}>
-                <h2 className={styles.titulo}>{titulo}</h2>
-                <img className={styles.icon} src={iconFavorito} alt='Icono favorito'/>
+                <Link className={styles.link} to={`/${id}`}>
+                    <h2 className={styles.titulo}>{titulo}</h2>
+                </Link>
+                <img 
+                    onClick={() => agregarFavorito({id, capa, titulo})}
+                    className={styles.icon} 
+                    src={icon} 
+                    alt='Icono favorito'
+                />
             </div>
         </div>
     )
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FavoritosProvider from "context/Favoritos";
 
 import Favoritos from "pages/Favoritos/Fovoritos";
 import Inicio from "pages/Inicio";
+import Player from "pages/Player/Player";
+
 import Footer from "components/Footer/Footer";
 import Header from "components/Header/Header";
 import Container from "components/Container/Container";
-import FavoritosProvider from "context/Favoritos";
+import NotFound from "pages/NotFound/NotFound";
 
 
 function AppRoutes(){
@@ -17,6 +20,8 @@ function AppRoutes(){
                     <Routes>
                         <Route path="/" element={<Inicio/>}></Route>
                         <Route path="/favoritos" element={<Favoritos/>}></Route>
+                        <Route path="/:id" element={<Player/>}></Route>
+                        <Route path="*" element={<NotFound/>}></Route>
                     </Routes>
                 </FavoritosProvider>
             </Container>
